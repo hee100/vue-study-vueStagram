c37dcc881de0338fbddbceed325b53badbf3e40c
hee100 <ggng200@naver.com>
2024-02-05 22:40:24 +0900

    coddingApple ~3-02

---
+++ src/components/Container.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+  <!-- <div v-for="(data, i) in postdata" :key = i>
+    {{data}}
+  </div> -->
+  <Post :data="data" v-for="(data,i) in postdata" :key="i"/>
+  <!-- <Post /> -->
+</div>
+</template>
+
+<script>
+import Post from './Post.vue';
+
+export default {
+  components: {
+    Post: Post,
+  }, 
+  props:{
+    postdata: Array,
+  }
+}
+</script>
+
+<style>
+
+</style>
