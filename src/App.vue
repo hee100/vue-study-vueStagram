36ddae722b93a7b59d2145f30bb878048a47b72f
hee100 <ggng200@naver.com>
2024-02-08 03:15:55 +0900

    coddingApple ~3-04

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
     <img src="./assets/logo.png" class="logo" />
   </div>
 
-  <Container :postdata="postdata"/>
+  <Container :step="step" :postdata="postdata"/>
   <button @click="more">더보기</button>
 
 
@@ -32,7 +32,8 @@ export default {
   data() {
     return {
       postdata : postdata,
-      count : 0
+      count : 0,
+      step : 2
     }
   },
   methods : {
@@ -42,7 +43,7 @@ export default {
         this.postdata.push(result.data);
         this.count++;
       })
-    }
+    },
   },
   components : {
     Container : Container,
